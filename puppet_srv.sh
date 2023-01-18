fa345e004dfbe7ca2c265490d7988dca9147a727
Nikolay Gorbunov <32635064+NickVG@users.noreply.github.com>
2023-01-18 22:02:34 +0300

    Update puppet_srv.sh

--- puppet_srv.sh
+++ puppet_srv.sh
@@ -1,7 +1,11 @@
 puppet-srv
 #!/bin/bash
+sudo apt install debian-archive-keyring 
+
 vim /etc/apt/sources.list
 echo "deb https://deb.debian.org/debian/               buster         main contrib non-free" >> /etc/apt/sources.list
+echo "deb https://security.debian.org/debian-security/ buster/updates main contrib non-free" >> /etc/apt/sources.list
+sudo apt update
 vim /etc/apt/preferences.d/smolensk
 apt update
 wget https://apt.puppet.com/puppet7-release-buster.deb
